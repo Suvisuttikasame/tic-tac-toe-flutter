0589b044ab6f806906d7369318e5463a829b3dba
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2024-03-13 23:38:45 +0700

    - [feat] create main menu btn + responsive

--- lib/screens/main_menu.dart
+++ lib/screens/main_menu.dart
@@ -1,12 +1,26 @@
 import "package:flutter/material.dart";
+import "package:tic_tac_toe/widgets/custom_button.dart";
 
 class MainMenu extends StatelessWidget {
   const MainMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('main menu')),
+    return Scaffold(
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CustomButton(onPressed: () {}, buttonLabel: 'create room'),
+            const SizedBox(
+              height: 20,
+            ),
+            CustomButton(onPressed: () {}, buttonLabel: 'join room'),
+          ],
+        ),
+      ),
     );
   }
 }
