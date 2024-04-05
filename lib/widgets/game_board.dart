73abaa3b75b21352a58fdf6951eb0031fb10800d
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2024-04-05 23:54:00 +0700

    - [fix] grid view overflow

--- lib/widgets/game_board.dart
+++ lib/widgets/game_board.dart
@@ -24,52 +24,52 @@ class _GameBoardState extends State<GameBoard> {
   @override
   Widget build(BuildContext context) {
     final roomData = Provider.of<RoomDataProvider>(context);
-    final height = MediaQuery.of(context).size.height;
 
-    return ConstrainedBox(
-      constraints: BoxConstraints(maxHeight: height * 0.6, maxWidth: 700),
-      child: GridView.builder(
-        itemCount: 9,
-        gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-        itemBuilder: (context, index) {
-          return AbsorbPointer(
-            absorbing: roomData.roomData['turn']['socketID'] == socketID,
-            child: GestureDetector(
-              onTap: () {
-                _socketMethod.getSocketID();
-                _socketMethod.onTap(
-                  context,
-                  index,
-                  roomData.roomData['turn']['playerType'],
-                  roomData.roomData['_id'],
-                );
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.white24, width: 2)),
-                child: Center(
-                  child: Text(
-                    roomData.dashBoardData[index],
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 100,
-                        fontWeight: FontWeight.bold,
-                        shadows: [
-                          Shadow(
-                            blurRadius: 30,
-                            color: roomData.dashBoardData[index] == 'X'
-                                ? Colors.blue
-                                : Colors.red,
-                          )
-                        ]),
+    return GridView.builder(
+      itemCount: 9,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+      ),
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        return AbsorbPointer(
+          absorbing: roomData.roomData['turn']['socketID'] == socketID,
+          child: GestureDetector(
+            onTap: () {
+              _socketMethod.getSocketID();
+              _socketMethod.onTap(
+                context,
+                index,
+                roomData.roomData['turn']['playerType'],
+                roomData.roomData['_id'],
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white24, width: 2),
+              ),
+              child: Center(
+                child: Text(
+                  roomData.dashBoardData[index],
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 100,
+                    fontWeight: FontWeight.bold,
+                    shadows: [
+                      Shadow(
+                        blurRadius: 30,
+                        color: roomData.dashBoardData[index] == 'X'
+                            ? Colors.blue
+                            : Colors.red,
+                      ),
+                    ],
                   ),
                 ),
               ),
             ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
